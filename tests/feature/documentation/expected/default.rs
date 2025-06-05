b742c3d41511cfcadbbdfa79ea561a6424c163b0
Bergmann89 <info@bergmann89.de>
2025-06-05 18:03:35 +0200

    Normalize doc attributes during rustfmt

--- tests/feature/documentation/expected/default.rs
+++ tests/feature/documentation/expected/default.rs
@@ -1,16 +1,16 @@
-#[doc = "SomeDetails doc string 1"]
-#[doc = "SomeDetails doc string 2"]
-#[doc = "This is a multi line comment with mixed content."]
-#[doc = "<div>"]
-#[doc = "    You could use for example HTML to apply some <b>formatting</b> to your documentation."]
-#[doc = "    Of cause this must be supported by your language, but for rust docs this should work :)"]
-#[doc = "</div>"]
+///SomeDetails doc string 1
+///SomeDetails doc string 2
+///This is a multi line comment with mixed content.
+///<div>
+///    You could use for example HTML to apply some <b>formatting</b> to your documentation.
+///    Of cause this must be supported by your language, but for rust docs this should work :)
+///</div>
 pub type SomeDetails = SomeDetailsType;
 #[derive(Debug)]
 pub struct SomeDetailsType {
-    #[doc = "Documentation for attributes is also possible."]
+    ///Documentation for attributes is also possible.
     pub test_attrib: Option<IntType>,
-    #[doc = "Unique user identifier. Range 1000-99999"]
+    ///Unique user identifier. Range 1000-99999
     pub id: IntType,
 }
 pub type IntType = i32;
