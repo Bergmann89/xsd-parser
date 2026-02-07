140e360cd2f25c5c76e3b9938bf0bed3bbd2deaf
Bergmann89 <info@bergmann89.de>
2026-02-07 23:39:42 +0100

    Add example project for the AIXM schema

---
+++ examples/aixm/build.rs
@@ -0,0 +1,137 @@
+//! This is a build script to generate the code for the `vsme` schema.
+
+use std::env::var;
+use std::fs::{create_dir_all, remove_dir_all};
+use std::path::PathBuf;
+
+use anyhow::{Context, Error};
+
+use xsd_parser::{
+    config::{GeneratorFlags, InterpreterFlags, OptimizerFlags, Schema},
+    generate_modules,
+    models::{
+        meta::{MetaType, MetaTypeVariant},
+        schema::MaxOccurs,
+        Naming,
+    },
+    traits::{NameBuilder as NameBuilderTrait, Naming as NamingTrait},
+    Config, IdentType, Name, TypeIdent,
+};
+
+fn main() -> Result<(), Error> {
+    let cargo_dir =
+        var("CARGO_MANIFEST_DIR").context("Missing `CARGO_MANIFEST_DIR` environment variable!")?;
+    let cargo_dir = PathBuf::from(cargo_dir)
+        .canonicalize()
+        .context("Missing environment variable `CARGO_MANIFEST_DIR`")?;
+    let schema_file = cargo_dir
+        .join("schema/aixm.aero/schema/5.2/5.2.0/message/AIXM_BasicMessage.xsd")
+        .canonicalize()
+        .context("Missing or invalid schema file!")?;
+
+    // This is almost the starting point defined in the main `[README.md]`.
+    let config = Config::default()
+        .with_schema(Schema::File(schema_file))
+        .with_interpreter_flags(InterpreterFlags::all() - InterpreterFlags::WITH_NUM_BIG_INT)
+        .with_optimizer_flags(OptimizerFlags::all() - OptimizerFlags::FLATTEN_COMPLEX_TYPES)
+        .with_generator_flags(GeneratorFlags::all())
+        .with_naming(CustomNaming::default())
+        .with_quick_xml()
+        .with_generate([(IdentType::Element, "message:AIXMBasicMessage")]);
+
+    // Generate the code based on the configuration above.
+    let modules = generate_modules(config).context("Error while generating code")?;
+
+    // Write the generated code to the module directory specified by Cargo.
+    let target_dir = cargo_dir.join("src/schema");
+    let _ = remove_dir_all(&target_dir);
+    create_dir_all(&target_dir).context("Unable to create `src/schema` directory!")?;
+    modules
+        .write_to_files(&target_dir)
+        .context("Error while writing generated code")?;
+
+    Ok(())
+}
+
+#[derive(Debug, Default)]
+struct CustomNaming(Naming);
+
+impl NamingTrait for CustomNaming {
+    fn clone_boxed(&self) -> Box<dyn NamingTrait> {
+        Box::new(Self(self.0.clone()))
+    }
+
+    fn builder(&self) -> Box<dyn NameBuilderTrait> {
+        self.0.builder()
+    }
+
+    fn unify(&self, s: &str) -> String {
+        self.0.unify(s)
+    }
+
+    fn make_type_name(&self, postfixes: &[String], ty: &MetaType, ident: &TypeIdent) -> Name {
+        if let MetaTypeVariant::Reference(ti) = &ty.variant {
+            if ident.name.is_generated() && ti.type_.name.is_named() {
+                let s = self.format_type_name(ti.type_.name.as_str());
+
+                if ti.max_occurs > MaxOccurs::Bounded(1) {
+                    return Name::new_generated(format!("{s}List"));
+                } else if ti.min_occurs == 0 {
+                    return Name::new_generated(format!("{s}Opt"));
+                }
+            }
+        }
+
+        let postfix = postfixes
+            .get(ident.type_ as usize)
+            .map_or("", |s| s.as_str());
+
+        let s = self.format_type_name(ident.name.as_str());
+
+        if s.ends_with(postfix) {
+            ident.name.clone()
+        } else {
+            Name::new_generated(format!("{s}{postfix}"))
+        }
+    }
+
+    fn make_unknown_variant(&self, id: usize) -> xsd_parser::Ident2 {
+        self.0.make_unknown_variant(id)
+    }
+
+    fn format_module_name(&self, s: &str) -> String {
+        self.0.format_module_name(s)
+    }
+
+    fn format_type_name(&self, s: &str) -> String {
+        match s {
+            "secondDefiningParameter" => "SecondDefiningParameterBase".to_string(),
+            s => self.0.format_type_name(s),
+        }
+    }
+
+    fn format_field_name(&self, s: &str) -> String {
+        self.0.format_field_name(s)
+    }
+
+    fn format_variant_name(&self, s: &str) -> String {
+        match s {
+            "+" => "Plus".to_string(),
+            "-" => "Minus".to_string(),
+            s if s.contains("UTC") => s
+                .replace("UTC:", "Utc")
+                .replace("UTC", "Utc")
+                .replace(':', "_")
+                .replace('+', "Plus")
+                .replace('-', "Minus"),
+            s => self.0.format_variant_name(s),
+        }
+    }
+
+    fn format_attribute_field_name(&self, s: &str) -> String {
+        let s = self.0.format_attribute_field_name(s);
+        let s = s.trim_end_matches('_');
+
+        format!("{s}_attr")
+    }
+}
---
+++ examples/aixm/src/main.rs
@@ -0,0 +1,26 @@
+//! Example how to generate code for the VSME schema and use it to deserialize an XML file.
+
+#[allow(clippy::all, dead_code, missing_docs, unreachable_pub, unused)]
+#[rustfmt::skip]
+mod schema;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use anyhow::{Context, Error};
+use xsd_parser_types::quick_xml::{DeserializeSync, IoReader, XmlReader};
+
+use schema::message::AixmBasicMessage;
+
+fn main() -> Result<(), Error> {
+    let input_file = File::open("xml/COCESNA_20170516_sample.xml")
+        .context("Unable to open example XML for reading")?;
+    let reader = BufReader::new(input_file);
+    let mut reader = IoReader::new(reader).with_error_info();
+    let obj = AixmBasicMessage::deserialize(&mut reader)
+        .context("Error while deserializing XML content")?;
+
+    println!("{obj:#?}");
+
+    Ok(())
+}
